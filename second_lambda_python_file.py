54e476a8430013a43c089269c75eb0df2e9c9037
sonia <soniakataria154@gmail.com>
2024-03-17 21:39:06 +0530

    second bucket work,

--- second_lambda_python_file.py
+++ second_lambda_python_file.py
@@ -3,5 +3,5 @@ import boto3
 def handler(event = None,context= None):
     s3_resource = boto3.resource('s3')
     s3_bucket = s3_resource.Bucket('this-got-created-via-terraform-sonia-16march2024')
-    s3_file_object = s3_bucket.Object('file_uploaded_via_terraform.json').get()
+    s3_file_object = s3_bucket.Object('file_uploaded_via_terraform.json').get()["Body"].read()
     print(s3_file_object)
