607fe1ace24c690ee418d7bdc0cc027004455345
sonia <soniakataria154@gmail.com>
2024-03-17 21:58:07 +0530

    all files

--- second_lambda_python_file.py
+++ second_lambda_python_file.py
@@ -4,4 +4,6 @@ def handler(event = None,context= None):
     s3_resource = boto3.resource('s3')
     s3_bucket = s3_resource.Bucket('this-got-created-via-terraform-sonia-16march2024')
     s3_file_object = s3_bucket.Object('file_uploaded_via_terraform.json').get()["Body"].read()
-    print(s3_file_object)
+    decoded_info = s3_file_object.decode()
+    print(f'decoded information: {decoded_info}')
+    print(f'type:{type(decoded_info)}')
