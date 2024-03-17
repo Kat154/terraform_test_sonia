a28c7606d9348bb2a3070d3893edf80df3d1cfa4
sonia <soniakataria154@gmail.com>
2024-03-17 20:58:42 +0530

    adding more changes

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
 
 
-data "aws_iam_policy_document" "policy_for_s3_access_terraform_sonia"{
+data "aws_iam_policy_document" "ability_for_lambda_to_assume_role_in_aws"{
   statement {
 //    actions = ["sts:AssumeRole"]
     actions = ["sts:AssumeRole"]
@@ -9,13 +9,18 @@ data "aws_iam_policy_document" "policy_for_s3_access_terraform_sonia"{
       type = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
+    effect = "Allow"
   }
 }
 resource "aws_iam_role" "role_for_lambda_to_access_sonia_terraform_created_bucket_16march2024"{
   name = "allow_lambda_to_access_bucket_sonia_terraform"
-assume_role_policy = data.aws_iam_policy_document.policy_for_s3_access_terraform_sonia.json
+assume_role_policy = data.aws_iam_policy_document.ability_for_lambda_to_assume_role_in_aws.json
 }
 
+//resourcce "aws_iam_role" "role_for_first_lambda_to_access_s3"{
+//  name = "role_for_first_lambda_to_access_s3_aws"
+//
+//}
 resource "aws_iam_policy" "s3_bucket_access_for_first_lambda_function"{
   name = "s3_bucket_access_for_first_lambda_function_via_terraform"
   description = "this is being done to attach my s3 bucket access to my lambda function so that i can complete first exercise"
@@ -26,35 +31,37 @@ data "aws_iam_policy_document" "policy_for_s3_access"{
   statement{
     effect = "Allow"
     actions = ["s3:*"]
-    resources = ["arn:aws:s3:::this-got-created-via-terraform-sonia-16march2024"]
+//    resources = ["arn:aws:s3:::this-got-created-via-terraform-sonia-16march2024"]
+    resources  = [aws_s3_bucket.aws_s3_sonia_terraform_created_bucket_16march2024.arn]
   }
+
 }
 
 resource "aws_iam_role_policy_attachment" "attach_s3_access_for_first_lambda_function" {
         role =  aws_iam_role.role_for_lambda_to_access_sonia_terraform_created_bucket_16march2024.name
         policy_arn = aws_iam_policy.s3_bucket_access_for_first_lambda_function.arn
 }
-
-resource "aws_iam_role" "role_for_second_lambda_to_access_s3"{
-  name = "role_for_second_lambda_function_to_access_s3"
-  assume_role_policy = data.aws_iam_policy_document.policy_for_s3_access_terraform_sonia.json
-}
-
-resource "aws_iam_policy" "policy_for_s3_access_for_second_lambda"{
-  name = "s3_bucket_access_for_second_lambda_function_via_terraform"
-  description = "to give access of our bucket to second lambda function"
-  policy = data.aws_iam_policy_document.policy_for_s3_access_for_second_lambda.json
-}
-
-data "aws_iam_policy_document" "policy_for_s3_access_for_second_lambda"{
-  statement{
-    effect = "Allow"
-    actions = ["s3:*"]
-    resources = ["arn:aws:s3:::this-got-created-via-terraform-sonia-16march2024"]
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "attach_s3_access_for_second_lambda_function"{
-  role = aws_iam_role.role_for_second_lambda_to_access_s3.name
-  policy_arn = aws_iam_policy.policy_for_s3_access_for_second_lambda.arn
-}
+//----------------------------------------------------------------------------
+//resource "aws_iam_role" "role_for_second_lambda_to_access_s3"{
+//  name = "role_for_second_lambda_function_to_access_s3"
+//  assume_role_policy = data.aws_iam_policy_document.ability_for_lambda_to_assume_role_in_aws.json
+//}
+//
+//resource "aws_iam_policy" "policy_for_s3_access_for_second_lambda"{
+//  name = "s3_bucket_access_for_second_lambda_function_via_terraform"
+//  description = "to give access of our bucket to second lambda function"
+//  policy = data.aws_iam_policy_document.policy_for_s3_access_for_second_lambda.json
+//}
+//
+//data "aws_iam_policy_document" "policy_for_s3_access_for_second_lambda"{
+//  statement{
+//    effect = "Allow"
+//    actions = ["s3:*"]
+//    resources = ["arn:aws:s3:::this-got-created-via-terraform-sonia-16march2024"]
+//  }
+//}
+//
+//resource "aws_iam_role_policy_attachment" "attach_s3_access_for_second_lambda_function"{
+//  role = aws_iam_role.role_for_second_lambda_to_access_s3.name
+//  policy_arn = aws_iam_policy.policy_for_s3_access_for_second_lambda.arn
+//}
--- lambda_function_first.tf
+++ lambda_function_first.tf
@@ -24,22 +24,22 @@ resource "aws_lambda_function" "first_lambda_function"{
 //  source = "/mnt/d/work/terraform/file_to_be_uploaded_to_s3_via_terraform.txt"
 //}
 
-data "archive_file" "second_lambda_file"{
-  type = "zip"
-  source_file = "second_lambda_python_file.py"
-  output_path = "second_lambda_python_file.zip"
-}
-resource "aws_lambda_function" "second_lambda_function"{
-  filename = "second_lambda_python_file.zip"
-  function_name = "second_lamdba_function_via_terraform"
-  role = aws_iam_role.role_for_second_lambda_to_access_s3.arn
-  handler = "second_lambda_python_file.handler"
-  source_code_hash = "data.archive_file.second_lambda_file.output_base64sha256"
-  runtime = "python3.11"
-  environment {
-    variables ={
-      author_name="sonia"
-      function_number = "this is second function"
-    }
-  }
-}
+//data "archive_file" "second_lambda_file"{
+//  type = "zip"
+//  source_file = "second_lambda_python_file.py"
+//  output_path = "second_lambda_python_file.zip"
+//}
+//resource "aws_lambda_function" "second_lambda_function"{
+//  filename = "second_lambda_python_file.zip"
+//  function_name = "second_lamdba_function_via_terraform"
+//  role = aws_iam_role.role_for_second_lambda_to_access_s3.arn
+//  handler = "second_lambda_python_file.handler"
+//  source_code_hash = "data.archive_file.second_lambda_file.output_base64sha256"
+//  runtime = "python3.11"
+//  environment {
+//    variables ={
+//      author_name="sonia"
+//      function_number = "this is second function"
+//    }
+//  }
+//}
